0ae0df422c5b6dd5ba5e649fcd0c71aa61d5d977
Cranberry-Juice <josepfamador2000@gmail.com>
2022-03-06 16:37:18 -0800

    Pre Decoupling of Ty, Tx

---
+++ JA/scripts/decouplingTx_ty.m
@@ -0,0 +1,25 @@
+nsamples = 50; % Number of samples for numerical estimation
+
+% Expected operating range of thurst vector deflectin angle.
+thMin = -15; % Degrees, 
+thMax = 15; % Degrees.
+
+TyMin = 1; % Ty/Weight. Minimum thrust expected.
+TyMax = 2; % Ty/Wight, Max Vertical thrust expected.
+
+theta = linspace(thMin, thMax, nsamples);
+Ty = linspace(TyMin, TyMax, nsamples);
+
+[TY, TH] = meshgrid(Ty, theta);
+
+Tx = TY .* tand(TH);
+
+surf(TY, TH, Tx)
+xlabel("T_y (T_y/W)")
+ylabel("\theta")
+zlabel('T_x')
+
+Thrust = sqrt(Tx.^2 + TY.^2);
+
+figure
+surf(Tx, TY, Thrust)
